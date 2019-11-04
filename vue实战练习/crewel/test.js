f5513e76589f6cf0d8f269e81359b1a61ca5b31a
W-GuiMin <1138598672@qq.com>
2019-11-04 20:55:06 +0800

    19.11.04.20

--- vue实战练习/crewel/test.js
+++ vue实战练习/crewel/test.js
@@ -1,22 +1,31 @@
 const https = require('https')
 const {
-    insert
+    insert, find
 } = require('./db')
-https.get('https://h5.ele.me/restapi/shopping/v2/entries?latitude=23.12908&longitude=113.264359&templates[]=main_template&templates[]=favourable_template&templates[]=svip_template&terminal=h5', (res) => {
+https.get('https://m.lagou.com/listmore.json?pageNo=4&pageSize=15', (res) => {
     res.setEncoding('utf8');
     let rawData = '';
     res.on('data', (chunk) => {
         rawData += chunk;
     });
     res.on('end', () => {
-        let entries = JSON.parse(rawData)[0].entries
-        console.log(entries)
-        entries.forEach(element => {
-            insert('element', [{
-                name: element.name
+        let result = JSON.parse(rawData).content.data.page.result
+        result.forEach(element => {
+            insert('company', [{
+                companyName: element.companyName,
+                positionName: element.positionName,
+                city: element.city,
+                createTime: element.createTime,
+                salary: element.salary,
+                companyLogo: 'http://www.lgstatic.com/' + element.companyLogo,//http://www.lgstatic.com/i/image2/M01/8F/91/CgoB5lug3MeASrcZAAAsHI8z3Ns453.png
+                companyName: element.companyName,
+                companyFullName: element.companyFullName
             }])
         });
     });
 }).on('error', (e) => {
     console.error(`出现错误: ${e.message}`);
 });
+
+let a = find('company')
+console.log(a)
